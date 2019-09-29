a2387a20f80c3c926f3634dab0f050ec8b1c5c7e
Sudarshan Kondgekar <skondgekar@yahoo.co.in>
2019-09-29 12:02:36 +0300

    Added simple text input on screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,69 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp())
+void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   final appTitle = "Hello You App";
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: appTitle,
-      home: MyHomePage(
-        title: appTitle
-      )
-    );
+        theme: ThemeData(primarySwatch: Colors.purple),
+        title: appTitle,
+        home: MyHomePage(title: appTitle));
   }
 }
 
-class MyHomePage extends StatelessWidget{
+class MyHomePage extends StatelessWidget {
   final String title;
   MyHomePage({Key key, this.title}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Hello You App",
+          textDirection: TextDirection.ltr,
+        ),
+      ),
+      body: HomePageContents(),
+    );
   }
+}
 
+class HomePageContents extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return UserInput();
+  }
+}
+
+class UserInput extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return UserInputState();
+  }
+}
+
+class UserInputState extends State<UserInput> {
+  String name = "";
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      padding: EdgeInsets.only(left: 20.0, right: 20.0),
+      child: Column(
+        children: <Widget>[
+          TextField(
+            onChanged: (String string) {
+              setState(() {
+                name = string;
+              });
+            },
+          )
+        ],
+      ),
+    );
+  }
 }
